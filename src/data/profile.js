203582c16d63baad2f5599ea26d0f41b6cbe18f9
rifqizheel-cmd <rifqizheel@gmail.com>
2026-06-08 09:59:40 +0700

    perubahan

--- src/data/profile.js
+++ src/data/profile.js
@@ -2,7 +2,7 @@ export const profile = {
   namaLengkap: "Muhammad Rifqi Alhilali",
   namaPanggilan: "Alhil",
   deskripsi: "Ana seorang pelajar SMA yang menyukai olahraga dan bercita-cita menjadi pengusaha. Ana senang mendalami bidang yang menarik perhatian saya, serta memiliki sifat yang cukup fleksibel dan mudah menyesuaikan diri dengan berbagai keadaan.",
-  fotoProfil: "/src/assets/react.svg",
+  fotoProfil: "/assets/react.svg",
   pendidikan: {
     institusi: "SMA IT HSI Boarding School",
     tahun: "2024 - 2027"
--- src/data/skills.js
+++ src/data/skills.js
@@ -3,36 +3,36 @@ export const skills = [
     id: 1,
     nama: "HTML",
     level: "Intermediate",
-    image: "/src/assets/HTML.png"
+    image: "/assets/HTML.png"
   },
   {
     id: 2,
     nama: "CSS",
     level: "Intermediate",
-    image: "/src/assets/CSS.png"
+    image: "/assets/CSS.png"
   },
   {
     id: 3,
     nama: "JavaScript",
     level: "Beginner",
-    image: "/src/assets/JS.png"
+    image: "/assets/JS.png"
   },
   {
     id: 4,
     nama: "React JS",
     level: "Beginner",
-    image: "/src/assets/REACT-JS.png"
+    image: "/assets/REACT-JS.png"
   },
   {
     id: 5,
     nama: "Canva",
     level: "Advanced",
-    image: "/src/assets/CANVA.png"
+    image: "/assets/CANVA.png"
   },
   {
     id: 6,
     nama: "Figma",
     level: "Intermediate",
-    image: "/src/assets/FIGMA.png"
+    image: "/assets/FIGMA.png"
   }
 ];
